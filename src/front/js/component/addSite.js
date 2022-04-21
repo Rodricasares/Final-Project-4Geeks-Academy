154b88d9ba830227c6eca3122b73aaead6a02ca3
Jose Medina Tortosa <josemedinatortosa@gmail.com>
2022-04-21 17:28:47 +0000

    estilo a addSite

--- src/front/js/component/addSite.js
+++ src/front/js/component/addSite.js
@@ -24,7 +24,8 @@ export const AddSite = (props) => {
   return (
     <div>
       <form>
-        <div class="mb-3">
+        <div class="container-sm bg-light rounded opacity-75 p-5 m-3">
+          <h1 className="m-3">Añadir nuevo Lugar</h1>
           <label for="name" class="form-label">
             Nombre del sitio
           </label>
@@ -36,80 +37,81 @@ export const AddSite = (props) => {
             placeholder="Escriba el nombre del lugar"
             onChange={handleChange}
           />
-        </div>
-        <div class="mb-3">
-          <label for="city" class="form-label">
-            Ciudad
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="city"
-            name="city"
-            placeholder="Escriba el nombre de la ciudad donde esta"
-            onChange={handleChange}
-          />
-        </div>
-        <div class="mb-3">
-          <label for="location" class="form-label">
-            localizacion
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="location"
-            name="location"
-            placeholder="Escriba la localizacion"
-            onChange={handleChange}
-          />
-        </div>
-        <div class="mb-3">
-          <label for="video" class="form-label">
-            Video
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="video"
-            name="video"
-            placeholder="Inserte el video"
-            onChange={handleChange}
-          />
-        </div>
-        <div class="mb-3">
-          <label for="image" class="form-label">
-            Imagen
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="image"
-            name="image"
-            placeholder="Inserte una imagen"
-            onChange={handleChange}
-          />
-        </div>
-        <div class="mb-3">
-          <label for="description" class="form-label">
-            Descripcion
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="description"
-            name="description"
-            placeholder="Escriba una descripcion del lugar"
-            onChange={handleChange}
-          />
-        </div>
 
-        <button
-          type="submit"
-          className="btn btn-primary"
-          onClick={handleSubmit}
-        >
-          Submit
-        </button>
+          <div class="mb-3">
+            <label for="city" class="form-label">
+              Ciudad
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="city"
+              name="city"
+              placeholder="Escriba el nombre de la ciudad donde esta"
+              onChange={handleChange}
+            />
+          </div>
+          <div class="mb-3">
+            <label for="location" class="form-label">
+              Localización
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="location"
+              name="location"
+              placeholder="Escriba la localizacion"
+              onChange={handleChange}
+            />
+          </div>
+          <div class="mb-3">
+            <label for="video" class="form-label">
+              Video
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="video"
+              name="video"
+              placeholder="Inserte el video"
+              onChange={handleChange}
+            />
+          </div>
+          <div class="mb-3">
+            <label for="image" class="form-label">
+              Imagen
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="image"
+              name="image"
+              placeholder="Inserte una imagen"
+              onChange={handleChange}
+            />
+          </div>
+          <div class="mb-3">
+            <label for="description" class="form-label">
+              Descripcion
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="description"
+              name="description"
+              placeholder="Escriba una descripcion del lugar"
+              onChange={handleChange}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </div>
       </form>
     </div>
   );
