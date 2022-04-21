df6be4bb6edaba9184b471784b95169777996f54
Rodricasares <rodrigocantudo@gmail.com>
2022-04-21 17:17:32 +0000

    Css de la pagina home,register y login

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -8,8 +8,8 @@ export const Login = () => {
   const [user, setUser] = useState({});
   let history = useHistory();
   return (
-    <div className="row login">
-      <div className="container">
+    <div className="row ">
+      <div className="containe login">
         <Navbar />
         <div className="row  justify-content-center">
           <div className="col-md-6 col-sm-4">
